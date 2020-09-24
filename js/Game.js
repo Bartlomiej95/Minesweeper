9fea221ad23a368198a6ca3aec81f1f4b0b375a7
Bartlomiej95 <panfilbartlomiej@gmail.com>
2020-09-24 10:22:13 +0200

    Generate cell on board
    Generate cell on board. Add event to click on cell and add flag
    functionality.

---
+++ js/Game.js
@@ -0,0 +1,123 @@
+import { Cell } from "./Cell.js";
+import { UI } from "./UI.js";
+
+class Game extends UI {
+  // # - konwencja, dodanie hasza sprawia, że metody są prywatne
+
+  #config = {
+    easy: {
+      cols: 8,
+      rows: 8,
+      mines: 10,
+    },
+    medium: {
+      cols: 16,
+      rows: 16,
+      mines: 40,
+    },
+    expert: {
+      cols: 16,
+      rows: 30,
+      mines: 99,
+    },
+  };
+
+  #numberOfRows = null;
+  #numberOfCols = null;
+  #numberOfMines = null;
+
+  //tablica reprezentująca ilość pól do gry, której ilość jest zależna od poziomu trudności
+  #cells = [];
+  #cellsElements = null;
+
+  #board = null;
+
+  initializeGame() {
+    this.#handleElements();
+    this.#newGame();
+  }
+
+  #newGame(
+    rows = this.#config.easy.rows,
+    cols = this.#config.easy.cols,
+    mines = this.#config.easy.mines
+  ) {
+    //domyślnie pokazujemy poziom łatwy
+    this.#numberOfRows = rows;
+    this.#numberOfCols = cols;
+    this.#numberOfMines = mines;
+
+    //zmieniamy wartość css cells-in-row tak aby plansza odpowiednio rozmieściła się na ekranie
+    this.#setStyles();
+
+    this.#generateCells();
+    this.#renderBoard();
+
+    this.#cellsElements = this.getElements(this.UISelectors.cell);
+    this.#addCellsEventListeners();
+  }
+
+  #handleElements() {
+    //chwytamy element do którego będziemy mogli wkładac komórki
+    this.#board = this.getElement(this.UISelectors.board);
+  }
+
+  #addCellsEventListeners() {
+    // lewy przycisk i prawy przycisk myszy na polach do gry - obsługa
+
+    this.#cellsElements.forEach((element) => {
+      element.addEventListener("click", this.#handleCellClick);
+      element.addEventListener("contextmenu", this.#handleCellContextMenu);
+    });
+  }
+
+  #generateCells() {
+    for (let row = 0; row < this.#numberOfRows; row++) {
+      this.#cells[row] = [];
+      for (let col = 0; col < this.#numberOfCols; col++) {
+        this.#cells[row].push(new Cell(col, row));
+      }
+    }
+  }
+
+  #renderBoard() {
+    this.#cells.flat().forEach((cell) => {
+      this.#board.insertAdjacentHTML("beforeend", cell.createElement());
+      cell.element = cell.getElement(cell.selector);
+    });
+  }
+
+  #handleCellClick = (e) => {
+    console.log("Klik lewy");
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    this.#cells[rowIndex][colIndex].revealCell();
+  };
+
+  #handleCellContextMenu = (e) => {
+    e.preventDefault(); //zapobiegamy wyświetlaniu menu kontekstowego na prawym przycisku myszy
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    const cell = this.#cells[rowIndex][colIndex];
+
+    if (cell.isReveal) return;
+    cell.toggleFlag();
+  };
+
+  #setStyles() {
+    document.documentElement.style.setProperty(
+      "--cells-in-row",
+      this.#numberOfCols
+    );
+  }
+}
+
+window.onload = function () {
+  const game = new Game();
+
+  game.initializeGame();
+};
